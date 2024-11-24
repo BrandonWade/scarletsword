1f4ac28c57298312e79e25e21a69fef2cea87705
Brandon Wade <brandon_wade2005@hotmail.com>
2024-11-23 22:07:28 -0600

    Add initial integration with sqlite.

---
+++ db/index.ts
@@ -0,0 +1,49 @@
+import {
+  enablePromise,
+  openDatabase,
+  SQLiteDatabase,
+} from 'react-native-sqlite-storage';
+import {
+  cardsTable,
+  cardFacesTable,
+  decksTable,
+  deckCardsTable,
+} from './schema';
+
+enablePromise(true);
+
+export async function connectToDatabase() {
+  return openDatabase(
+    { name: 'scarletsword.db', location: 'default' },
+    () => console.log('Successfully connected to database'),
+    error => {
+      console.error(error);
+      throw Error('Could not connect to database');
+    },
+  );
+}
+
+async function createTables(db: SQLiteDatabase) {
+  try {
+    await db.executeSql(cardsTable);
+    await db.executeSql(cardFacesTable);
+    await db.executeSql(decksTable);
+    await db.executeSql(deckCardsTable);
+    console.log('Successfully created tables');
+  } catch (error) {
+    console.error(error);
+    throw Error('Failed to create tables');
+  }
+}
+
+async function init() {
+  try {
+    const db = await connectToDatabase();
+    await createTables(db);
+    console.log('Database initialized successfully');
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+init();
---
+++ db/utils.ts
@@ -0,0 +1,21 @@
+import { connectToDatabase } from './index';
+import {
+  cardsTable,
+  cardFacesTable,
+  decksTable,
+  deckCardsTable,
+} from './schema';
+
+export async function resetDB() {
+  const db = await connectToDatabase();
+
+  await db.executeSql('DROP TABLE cards;');
+  await db.executeSql('DROP TABLE card_faces;');
+  await db.executeSql('DROP TABLE decks;');
+  await db.executeSql('DROP TABLE deck_cards;');
+
+  await db.executeSql(cardsTable);
+  await db.executeSql(cardFacesTable);
+  await db.executeSql(decksTable);
+  await db.executeSql(deckCardsTable);
+}
