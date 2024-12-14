bf8a9fbc46d7e94725fb57bccfcef60ba7a20e8a
Brandon Wade <brandon_wade2005@hotmail.com>
2024-12-14 01:33:21 -0600

    Add input styles.

--- src/components/common/TextAreaField/styles.ts
+++ src/components/common/TextAreaField/styles.ts
@@ -2,12 +2,21 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   input: {
+    height: 100,
     padding: 6,
     fontSize: 16,
-    borderColor: 'black',
-    borderStyle: 'solid',
+    borderRadius: 5,
     borderWidth: 1,
-    height: 100,
+    borderColor: '#33333322',
+    backgroundColor: 'white',
     verticalAlign: 'top',
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.05,
+    shadowRadius: 0.84,
+    elevation: 2,
   },
 });
--- src/components/common/TextInputField/styles.ts
+++ src/components/common/TextInputField/styles.ts
@@ -4,8 +4,17 @@ export default StyleSheet.create({
   input: {
     padding: 6,
     fontSize: 16,
-    borderColor: 'black',
-    borderStyle: 'solid',
+    borderRadius: 5,
     borderWidth: 1,
+    borderColor: '#33333322',
+    backgroundColor: 'white',
+    shadowColor: '#000000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.05,
+    shadowRadius: 0.84,
+    elevation: 2,
   },
 });
